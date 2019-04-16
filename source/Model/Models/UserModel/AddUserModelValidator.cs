603ddb2e25e8803d742a594d878ffaf84641c598
Rafael Ferreira Garcia <rafaelfgx@gmail.com>
2019-04-15 21:00:00 -0300

    .

---
+++ source/Model/Models/UserModel/AddUserModelValidator.cs
@@ -0,0 +1,14 @@
+using FluentValidation;
+
+namespace DotNetCoreArchitecture.Model
+{
+    public sealed class AddUserModelValidator : UserModelValidator<AddUserModel>
+    {
+        public AddUserModelValidator()
+        {
+            RuleFor(x => x.SignIn).NotNull().NotEmpty();
+            RuleFor(x => x.SignIn.Login).NotNull().NotEmpty();
+            RuleFor(x => x.SignIn.Password).NotNull().NotEmpty();
+        }
+    }
+}
